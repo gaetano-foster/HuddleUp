603e3b44bf88f8565622e941fe5f7953a9363479
Gaetano Foster <gaetanox109@gmail.com>
2025-04-06 15:51:06 -0400

    Switched to OpenGL Rendering. TODO: Render planes entirely on GPU

--- HuddleUp/src/main.c
+++ HuddleUp/src/main.c
@@ -3,9 +3,12 @@
 #include <stdint.h>
 #include <time.h>
 #include <math.h>
+#include <Windows.h>
+#include <gl/GL.h>
 
 #define SCR_W			800
 #define SCR_H			600
+#define ASPECT_RATIO	1.33f
 
 // data types simply for ease of typing 
 typedef uint8_t byte_t;
@@ -60,6 +63,13 @@ typedef struct state_t
 	plane_t ceiling;
 } state_t;
 
+// wrapper around sdl window and renderer for window management
+typedef struct display_t
+{
+	SDL_Window *win;
+	SDL_GLContext context;
+} display_t;
+
 SDL_Color
 GetPixelFromSurface(
 	SDL_Surface *surface,
@@ -100,13 +110,6 @@ GetPixelFromSurface(
 	return rgb;
 }
 
-// wrapper around sdl window and renderer for window management
-typedef struct display_t
-{
-	SDL_Window *win;
-	SDL_Renderer *ren;
-} display_t;
-
 bool_t
 ConstructDisplay(
 	display_t *display,
@@ -114,14 +117,13 @@ ConstructDisplay(
 	dword_t width,
 	dword_t height
 ) {
-	if (!(display->win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0))) {
+	if (!(display->win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL))) {
 		fprintf(stderr, "Couldn't initialize display.\n%s", SDL_GetError());
 		return 0;
 	}
 
-	if (!(display->ren = SDL_CreateRenderer(display->win, -1, 0))) {
-		fprintf(stderr, "Couldn't initialize display.\n%s", SDL_GetError());
-		return 0;
+	if (!(display->context = SDL_GL_CreateContext(display->win))) {
+		fprintf(stderr, "Couldn't initialize OpenGL.\n%s", SDL_GetError());
 	}
 
 	return 1;
@@ -131,21 +133,20 @@ void
 DestroyDisplay(
 	display_t *display
 ) {
+	SDL_GL_DeleteContext(display->context);
 	SDL_DestroyWindow(display->win);
-	SDL_DestroyRenderer(display->ren);
 }
 
 bool_t
 CreatePlane(
 	plane_t *plane,
-	float near,
-	float far,
+	float near_dist,
+	float far_dist,
 	bool_t orientation,
 	const char *tex_path
-)
-{
-	plane->local_near = near;
-	plane->local_far = far;
+) {
+	plane->local_near = near_dist;
+	plane->local_far = far_dist;
 	plane->orientation = orientation;
 	if (!(plane->tex = SDL_LoadBMP(tex_path))) {
 		fprintf(stderr, "Error: Couldn't load texture from %s.\n%s\n", tex_path, SDL_GetError());
@@ -163,6 +164,7 @@ CreatePlane(
 			plane->raw_tex[y * plane->tex_width + x] = GetPixelFromSurface(plane->tex, x, y);
 		}
 	}
+
 	SDL_FreeSurface(plane->tex);
 	
 	return 1;
@@ -171,17 +173,111 @@ CreatePlane(
 void
 DestroyPlane(
 	plane_t *plane
-)
-{
+) {
 	free(plane->raw_tex);
+}
+
+bool_t
+RenderPlane(
+	//SDL_Renderer *ren,
+	plane_t plane,
+	float cx,	// camera x coord
+	float cy,	// camera y coord
+	float ca,	// camera angle in radians
+	float fov	// camera field of view in radians 
+) {
+	// positions of start and end points on near and far plane
+	float far_1_x = (cx + cosf(ca - fov/2) * plane.local_far);
+	float far_1_y = (cy + sinf(ca - fov/2) * plane.local_far);
+
+	float near_1_x = (cx + cosf(ca - fov/2) * plane.local_near);
+	float near_1_y = (cy + sinf(ca - fov/2) * plane.local_near);
+
+	float far_2_x = (cx + cosf(ca + fov/2) * plane.local_far);
+	float far_2_y = (cy + sinf(ca + fov/2) * plane.local_far);
+
+	float near_2_x = (cx + cosf(ca + fov/2) * plane.local_near);
+	float near_2_y = (cy + sinf(ca + fov/2) * plane.local_near);
+
+
+	int corner_1_x = 0;
+	int corner_1_y = 0;
+
+	int corner_2_x = 0;
+	int corner_2_y = 0;
+
+	int corner_3_x = 0;
+	int corner_3_y = 0;
+
+	int corner_4_x = 0;
+	int corner_4_y = 0;
+
+	for (int y = 1; y < SCR_H / 2; y++) {
+		float sample_depth = (float)y / ((float)SCR_H / 2.0f); // y = depth; closer to vanishing point should be smaller
+
+		float start_x = (far_1_x - near_1_x) / sample_depth + near_1_x; // ray start position
+		float start_y = (far_1_y - near_1_y) / sample_depth + near_1_y;
+
+		float   end_x = (far_2_x - near_2_x) / sample_depth + near_2_x;  // ray end position
+		float   end_y = (far_2_y - near_2_y) / sample_depth + near_2_y;
+
+		for (int x = 1; x < SCR_W; x++) {
+			float sample_width = (float)x / (float)SCR_W;
+
+			float sample_x = (end_x - start_x) * sample_width + start_x;
+			float sample_y = (end_y - start_y) * sample_width + start_y;
+
+			SDL_Color rgb;
+
+			int tx = (int)(sample_x * (plane.tex_width));
+			int ty = (int)(sample_y * (plane.tex_height));
+			int dy = plane.orientation ? y + SCR_H / 2 : SCR_H - (y + SCR_H / 2); // determine pixel y position based on plane orientation and distance from camera
+
+			if (isnan(tx) || isnan(ty)) {
+				rgb = (SDL_Color) { 0, 0, 0 };
+			}
+			else if (tx < 0 || ty < 0) {
+				rgb = (SDL_Color) { 0, 0, 0 };
+			}
+			else if (tx > plane.tex_width || ty > plane.tex_height) {
+				rgb = (SDL_Color) { 0, 0, 0 };
+			}
+			else {
+				rgb = plane.raw_tex[ty * plane.tex_width + tx];
+				SDL_Color red = (SDL_Color) { 255, 0, 0 };
+
+				if (tx == 0 && ty == 0) {
+					corner_1_x = x;
+					corner_1_y = dy;
+				}
+				else if (tx == plane.tex_width && ty == 0) {
+					corner_2_x = x;
+					corner_2_y = dy;
+				}
+				else if (tx == 0 && ty == plane.tex_height) {
+					corner_3_x = x;
+					corner_3_y = dy;
+				}
+				else if (tx == plane.tex_width && ty == plane.tex_height) {
+					corner_4_x = x;
+					corner_4_y = dy;
+				}
+			}
+			// TODO: Implement real projection and gpu rendering
+			glColor3ub(rgb.r, rgb.g, rgb.b); 
+			glPointSize(1.0f);
+			glBegin(GL_POINTS);
+			glVertex2i(x, dy);
+			glEnd();
+		}
+	}
 }
 
 bool_t
 InitGameLoop(
 	state_t *state,
 	int32_t fps
-)
-{
+) {
 	state->gameloop.running = 1;
 	state->gameloop.last_time = clock();
 	state->gameloop.time = 0;
@@ -197,8 +293,7 @@ InitGameLoop(
 void
 InitCamera(
 	state_t *state
-)
-{
+) {
 	state->camera.x = 0;
 	state->camera.y = 0;
 	state->camera.a = 0.1f;
@@ -212,6 +307,13 @@ Init(
 	SDL_Init(SDL_INIT_EVERYTHING);
 	InitCamera(state);
 	InitGameLoop(state, 72);
+	// opengl init
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, SCR_W, SCR_H, 0, -1, 1); // left, right, bottom, top, near, far
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
 	if (!CreatePlane(&(state->floor), 0.001f, 0.06f, 1, "res/gfx/floor.bmp")) {
 		return 0;
 	}
@@ -273,74 +375,17 @@ Tick(
 }
 
 bool_t
-RenderPlane(
-	SDL_Renderer *ren,
-	plane_t plane,
-	float cx,	// camera x coord
-	float cy,	// camera y coord
-	float ca,	// camera angle in radians
-	float fov	// camera field of view in radians 
-) {
-	// positions of start and end points on near and far plane
-	float far_1_x = (cx + cosf(ca - fov/2) * plane.local_far);
-	float far_1_y = (cy + sinf(ca - fov/2) * plane.local_far);
-
-	float near_1_x = (cx + cosf(ca - fov/2) * plane.local_near);
-	float near_1_y = (cy + sinf(ca - fov/2) * plane.local_near);
-
-	float far_2_x = (cx + cosf(ca + fov/2) * plane.local_far);
-	float far_2_y = (cy + sinf(ca + fov/2) * plane.local_far);
-
-	float near_2_x = (cx + cosf(ca + fov/2) * plane.local_near);
-	float near_2_y = (cy + sinf(ca + fov/2) * plane.local_near);
-
-	for (int y = 1; y < SCR_H / 2; y++) {
-		float sample_depth = (float)y / ((float)SCR_H / 2.0f); // y = depth; closer to vanishing point should be smaller
-
-		float start_x = (far_1_x - near_1_x) / sample_depth + near_1_x; // ray start position
-		float start_y = (far_1_y - near_1_y) / sample_depth + near_1_y;
-
-		float   end_x = (far_2_x - near_2_x) / sample_depth + near_2_x;  // ray end position
-		float   end_y = (far_2_y - near_2_y) / sample_depth + near_2_y;
-
-		for (int x = 1; x < SCR_W; x++) {
-			float sample_width = (float)x / (float)SCR_W;
-
-			float sample_x = (end_x - start_x) * sample_width + start_x;
-			float sample_y = (end_y - start_y) * sample_width + start_y;
-
-			SDL_Color rgb;
-			int tx = sample_x * plane.tex_width;
-			int ty = sample_y * plane.tex_height;
-
-			if (isnan(tx) || isnan(ty)) {
-				rgb = (SDL_Color) { 0, 0, 0 };
-			}
-			else if (tx < 0 || ty < 0) {
-				rgb = (SDL_Color) { 0, 0, 0 };
-			}
-			else if (tx > plane.tex_width || ty > plane.tex_height) {
-				rgb = (SDL_Color) { 0, 0, 0 };
-			}
-			else {
-				rgb = plane.raw_tex[ty * plane.tex_width + tx];
-			}
-			SDL_SetRenderDrawColor(ren, rgb.r, rgb.g, rgb.b, 255);
-			int dy = plane.orientation ? y + SCR_H / 2 : SCR_H - (y + SCR_H / 2); // determine pixel y position based on plane orientation and distance from camera
-			SDL_RenderDrawPoint(ren, x, dy);
-		}
-	}
-}
-
-bool_t
 Render(
 	display_t *display,
 	state_t *state 
 ) {
-	SDL_SetRenderDrawColor(display->ren, 0, 0, 0, 255);
-	SDL_RenderClear(display->ren); 
-	RenderPlane(display->ren, state->floor, state->camera.x, state->camera.y, state->camera.a, 3.14f / 1.5f);
-	SDL_RenderPresent(display->ren);
+	glViewport(0, 0, SCR_W, SCR_H);
+	glClearColor(0.f, 0.f, 0.f, 1.f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	RenderPlane(state->floor, state->camera.x, state->camera.y, state->camera.a, 1.57f);
+
+	SDL_GL_SwapWindow(display->win);
 	return 1;
 }
 
